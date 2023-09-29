5cf46675b69826a7b030033ec30b9344e1138017
Alan <alan.guzman12333@gmail.com>
2023-09-29 17:34:55 -0600

    version 2

--- src/components/Menu/MenuComponent.vue
+++ src/components/Menu/MenuComponent.vue
@@ -2,7 +2,32 @@
 import "./Menu.css";
 
 export default {
-    name: "MenuComponent"
+    name: "MenuComponent",
+    data() {
+        return {
+            isMenuOpen: false,
+            expands:false
+        }
+    },
+    methods: {
+        Openmenu() {
+            if (this.isMenuOpen === false) {
+                this.isMenuOpen = true;
+            }
+            else {
+                this.isMenuOpen = false;
+            }
+        },
+        Expand()
+        {
+            if (this.expands === false) {
+                this.expands = true;
+            }
+            else {
+                this.expands = false;
+            }
+        }
+    },
 }
 </script>
 
@@ -52,8 +77,53 @@ export default {
                     </div>
                 </div>
                 <button class="button-coti">
-                    Cotizar <font-awesome-icon class="icon-button" icon="fa-solid fa-circle-arrow-right"/>
+                    Cotizar <font-awesome-icon class="icon-button" icon="fa-solid fa-circle-arrow-right" />
                 </button>
+            </div>
+        </div>
+        <div class="menu-movil">
+            <div class="menu-content">
+                <div class="image-content">
+                    <img src="../../assets/logo.png" alt="">
+                </div>
+                <div class="content-menu-button">
+                    <div class="menu_button" :class="{ menu_open: isMenuOpen == true }" @click="Openmenu">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="item-menu-movil-content" :class="{ show_menu: isMenuOpen == true }">
+                <div class="item-menu-movil">
+                    <router-link to="/">Inicio</router-link>
+                </div>
+                <div class="item-menu-movil">
+                    <span @click="Expand">Servicios</span>
+                    <div class="services-content-menu" :class="{expand:expands==true}">
+                        <div class="dropdown-item-menu">
+                            <p>Impresión Digital</p>
+                        </div>
+                        <div class="dropdown-item-menu">
+                            <p>Impresión Offset</p>
+                        </div>
+                        <div class="dropdown-item-menu">
+                            <p>Tarjetas de presentación</p>
+                        </div>
+                        <div class="dropdown-item-menu">
+                            <p>Sublimación</p>
+                        </div>
+                        <div class="dropdown-item-menu">
+                            <p>Diseño Digital</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-menu-movil">
+                    <router-link to="/acerca-de">Acerca de</router-link>
+                </div>
+                <div class="item-menu-movil">
+                    <router-link to="/contacto">Contactanos</router-link>
+                </div>
             </div>
         </div>
     </div>
