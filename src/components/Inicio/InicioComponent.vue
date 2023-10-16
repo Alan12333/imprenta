09af1349f4d89435be6553f93e52aed2f61862b8
alan12333 <alan.guzman12333@gmail.com>
2023-10-16 12:54:25 -0600

    nuevos elementos

--- src/components/Inicio/InicioComponent.vue
+++ src/components/Inicio/InicioComponent.vue
@@ -49,53 +49,85 @@ export default {
 
     <div class="inicio-section-2">
         <div class="text-margin"></div>
-        <div class="w-75 m-auto">
+        <div class="w-100 m-auto">
+            <h1 class="text-trans">MAS SOLICITADO</h1>
             <h3 class="fs-1 text-bold text-center blueblack">MÁS SOLICITADO</h3>
         </div><br>
         <div class="content mt-5">
             <div class="rw">
                 <div class="card-service sup-4-s">
                     <div class="cont-100 flex mt-3 mb-3">
-                        <div class="cont-25 mr-auto">
+                        <div class="cont-30 mr-auto">
                             <img src="../../assets/icons/ofset.png" alt="">
                         </div>
-                        <div class="cont-70 mr-auto">
-                            <p class="fs-6 fw-bold mt-3">IMPRESIÓN OFFSET</p>
+                        <div class="cont-65 mr-auto">
+                            <p class="article-text fw-bold mt-3">IMPRESIÓN OFFSET</p>
                         </div>
+                    </div>
+                    <br>
+                    <div class="cont-90 m-auto">
+                        <p class="fs-6 text-justify">
+                            La impresión offset es ideal para grandes cantidades
+                            de materiales de alta calidad, como revistas y catálogos.
+                            La tinta se transfiere de cilindro a papel,
+                            logrando colores vibrantes y detalles precisos.
+                        </p>
+
                     </div>
                 </div>
                 <div class="card-service sup-4-s card-down">
                     <div class="cont-100 flex mt-3 mb-3">
-                        <div class="cont-25 mr-auto">
+                        <div class="cont-30 mr-auto">
                             <img src="../../assets/icons/design.png" alt="">
                         </div>
-                        <div class="cont-70 mr-auto">
-                            <p class="ts-6 fw-bold mt-3">DISEÑO GRÁFICO</p>
+                        <div class="cont-65 mr-auto">
+                            <p class="article-text fw-bold mt-3">DISEÑO GRÁFICO</p>
                         </div>
+                    </div>
+                    <div class="cont-90 m-auto">
+                        <p class="fs-6 text-justify">
+                            El diseño gráfico es esencial para la identidad de tu empresa. Creamos logotipos, materiales de
+                            marketing y visuales cohesivos que comunican tu marca y atraen a tu audiencia.
+                        </p>
                     </div>
                 </div>
                 <div class="card-service sup-4-s">
                     <div class="cont-100 flex mt-3 mb-3">
-                        <div class="cont-25 mr-auto">
+                        <div class="cont-30 mr-auto">
                             <img src="../../assets/icons/playera.png" alt="">
                         </div>
-                        <div class="cont-70 mr-auto">
-                            <p class="ts-6 fw-bold mt-3">PERSONALIZACIÓN</p>
+                        <div class="cont-65 mr-auto">
+                            <p class="article-text fw-bold mt-3">PERSONALIZACIÓN</p>
                         </div>
+                    </div>
+                    <div class="cont-90 m-auto">
+                        <p class="fs-6 text-justify">
+                            La personalización es esencial para tu empresa. Ofrecemos productos únicos y personalizados,
+                            como tazas, llaveros, plumas, playeras y más, que resaltan tu marca y satisfacen las necesidades
+                            de tu audiencia.
+                        </p>
                     </div>
                 </div>
                 <div class="card-service card-down sup-4-s">
                     <div class="cont-100 flex mt-3 mb-3">
-                        <div class="cont-25 mr-auto">
+                        <div class="cont-30 mr-auto">
                             <img src="../../assets/icons/trazador.png" alt="">
                         </div>
-                        <div class="cont-70 mr-auto">
-                            <p class="ts-6 fw-bold mt-3">IMPRESIÓN DIGITAL</p>
+                        <div class="cont-65 mr-auto">
+                            <p class="article-text fw-bold mt-3">IMPRESIÓN DIGITAL</p>
                         </div>
+                    </div>
+                    <div class="cont-90 m-auto">
+                        <p class="fs-6 text-justify">
+                            La impresión digital destaca por su versatilidad. Ofrecemos servicios de impresión digital de
+                            alta calidad para brindar resultados nítidos y rápidos, resaltando tu marca con eficacia.
+                        </p>
                     </div>
                 </div>
             </div>
+            <div class="separator"></div>
         </div>
+
     </div>
 </template>
 
